fd9f00bb298d677a46cd5b9a9b644e5948941eef
Vinicius Pereira <vinicius.pereira@gotobiz.com.br>
2024-10-22 23:22:24 -0300

    Tratando erros com excessão personalizada

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -14,9 +14,49 @@
             pecas = new Peca[linhas, colunas];
         }
 
-        public Peca peca(int linha, int coluna) 
+        public Peca peca(Posicao pos) 
         {
-            return pecas[linha, coluna];
+            return pecas[pos.linha, pos.coluna];
+        }
+
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+
+        }
+
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linha < 0 || pos.linha >= linhas || pos.coluna >= colunas) 
+            {
+                return true;
+            }
+
+            return true;
+            
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+
+                throw new TabuleiroException("Posição Inálida");
+            }
         }
     }
+
 }
